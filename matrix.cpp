1d25914e905ec0dd76b3baa465d2888842eaf991
Harlan Williams <hrw@ku.edu>
2025-03-26 09:21:35 -0500

    add comments to explain how to implement

--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,17 @@
 #include "matrix.hpp"
+
+// implementation of functions declared in matrix.hpp goes here
+// function definitions for a class have their names prefixed with
+// the class name and "::"
+
+// e.g. for the constructor:
+
+Matrix::Matrix(size_t N) {
+    // initialize an empty NxN matrix
+}
+
+// e.g. for a member function:
+
+void Matrix::print_matrix() const {
+    // print out the matrix
+}
